e1b692bd913cb05b2001ffe29e9a7121aa175c4f
mystm <liushui_fei@126.com>
2016-08-25 19:49:47 +0800

    Modify pc.py

--- pc.py
+++ pc.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-s = "风朝"
-
-for i in s:
-    b = bytes(i,encoding='utf-8')
-    print(b)
-    s1 = str(b,encoding='utf-8')
+user = "熊飞"
+for ct in user:
+    print(ct)
+    bt = bytes(ct,encoding='utf8')
+    print(bt)
+    for c in bt:
+        print(c)
+    s1 = str(bt,encoding="utf8")
     print(s1)
